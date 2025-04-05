551474b5d575171897e6316d25f81822eca60be8
CamaradaCoco <nsmith196@student.cscc.edu>
2025-04-05 03:30:57 -0400

    More functionality

---
+++ Lab12.py
@@ -0,0 +1,58 @@
+# Lab 12 / #1
+
+import sys
+import pygame as pg
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pg.init()
+        self.clock = pg.time.Clock()
+        self.settings = Settings()
+
+        self.screen = pg.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pg.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            self.clock.tick(60)
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                sys.exit()
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pg.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pg.KEYUP:
+                if event.key == pg.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pg.K_LEFT:
+                    self.ship.moving_left = False
+
+    
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pg.display.flip()
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
